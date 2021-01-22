737395878ceff1844bd83536941a692c82d6ea1f
Kristoffer Davidsen <577870@stud.hvl.no>
2021-01-22 18:44:34 +0100

    init =)

---
+++ traffic_light.cpp
@@ -0,0 +1,28 @@
+﻿#include "traffic_light.h"
+
+
+traffic_light::traffic_light(const int state, int xpos, int ypos, HDC hdc)
+{
+	this->xpos_ = xpos;
+	this->ypos_ = ypos;
+	this->hdc_ = hdc;
+	this->state_ = state;
+}
+
+void traffic_light::draw_initial() const
+{
+	const HGDIOBJ stockbrush = GetStockObject(DC_BRUSH);
+    SelectObject(hdc_, stockbrush);
+    SetDCBrushColor(hdc_, RGB(0,0,0));
+	Rectangle(hdc_, xpos_,ypos_,xpos_ - 50,ypos_ - 150);
+    
+    SetDCBrushColor(hdc_, RGB(255,0,0));
+    Ellipse(hdc_, xpos_ - 5, ypos_ - 105, xpos_ - 45, ypos_ - 145);
+
+    SetDCBrushColor(hdc_, RGB(255,255,0));
+    Ellipse(hdc_, xpos_ - 5, ypos_ - 55, xpos_ - 45, ypos_ - 95);
+
+    SetDCBrushColor(hdc_, RGB(0,255,0));
+    Ellipse(hdc_, xpos_ - 5, ypos_ - 5, xpos_ - 45, ypos_ - 45);
+
+}
---
+++ traffic_light.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <Windows.h>
+
+class traffic_light
+{
+	int xpos_; //left pos
+	int ypos_; //top pos
+	int state_;
+	HDC hdc_;
+public:
+	traffic_light(int state, int xpos, int ypos, HDC hdc);
+	void draw_initial() const;
+};
